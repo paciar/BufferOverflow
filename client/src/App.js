7c4b345960d03e06913e2a1051feb37bce724a62
paciar <paciar04@gmail.com>
2024-06-05 00:27:01 -0400

    Configured boilerplate code and packages

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './stylesheets/App.css';
+import BufferOverflow from './components/BufferOverflow';
+
+function App() {
+  return (
+    <div className="buffer-overflow-wrapper">
+      <BufferOverflow />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/BufferOverflow.js
@@ -0,0 +1,5 @@
+export default function BufferOverflow() {
+    return (
+      <h1> Welcome to BufferOverflow! </h1>
+    );
+  }
