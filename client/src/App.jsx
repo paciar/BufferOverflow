6a258fca4f3dda9c3c4ebf72f2eaae35e62fb7c4
paciar <paciar04@gmail.com>
2024-06-05 15:30:22 -0400

    Reconfigured server directory and used Vite for client directory

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='BufferOverflow-wrapper'>
+      <h1>Welcome to BufferOverflow!</h1>
+    </div>
+  )
+}
+
+export default App
