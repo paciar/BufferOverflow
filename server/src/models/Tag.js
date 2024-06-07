3a7b723f0eed547c6838d77c5c9efd54c5d7caeb
paciar <paciar04@gmail.com>
2024-06-06 21:30:07 -0400

    Added description field to tags schema

--- server/src/models/Tag.js
+++ server/src/models/Tag.js
@@ -6,6 +6,7 @@ var Schema = mongoose.Schema;
 /**
  * Tag
  * - name: The name of the tag.
+ * - description: A brief description of the tag.
  * - owner: The user who created the tag.
  */
 
@@ -16,7 +17,12 @@ var TagSchema = new Schema({
         required: true,
         maxLength: 20
     },
-
+    description: // Previously not included
+    {
+        type: String,
+        required: true,
+        maxLength: 100
+    },
     // Previously included references to questions, but will be easier to query them separately.
     // i.e. Question.findAll({tags: tag._id}).sort({datePosted: -1})
 
