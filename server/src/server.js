c3a325c21e31bc2b982b0f072040784ecf47687c
paciar <paciar04@gmail.com>
2024-06-06 21:30:41 -0400

    Added code to start server on port 8000, will need to later configure .env file

--- server/src/server.js
+++ server/src/server.js
@@ -1,1 +1,13 @@
-// Code to start the server
+// #region SERVER CODE
+/***************************** SERVER CODE STARTS HERE *****************************/
+const express = require('express');
+
+const app = express();
+const port = 8000;
+
+// Start express server
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+/***************************** SERVER CODE ENDS HERE *****************************/
+// #endregion SERVER CODE
